8def80ab260cdf2afa2d49658a84d2e38757ea33
kvanklei <kaylavk@yahoo.com>
2024-12-14 12:57:34 -0600

    first commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,158 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+// defining right motors
+  private CANSparkMax r_motor1 = new CANSparkMax (0, MotorType.kBrushless);
+  private CANSparkMax r_motor2 = new CANSparkMax (1, MotorType.kBrushless);
+  private CANSparkMax r_motor3 = new CANSparkMax (2, MotorType.kBrushless);
+// defining left motors
+  private CANSparkMax l_motor1 = new CANSparkMax (3, MotorType.kBrushless);
+  private CANSparkMax l_motor2 = new CANSparkMax (4, MotorType.kBrushless);
+  private CANSparkMax l_motor3 = new CANSparkMax (5, MotorType.kBrushless);
+
+//joysticks
+  private Joystick r_joystick = new Joystick(1);
+  private Joystick l_joystick = new Joystick(0);
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+
+  //setting idlemode
+  r_motor1.setIdleMode(IdleMode.kBrake);
+  r_motor2.setIdleMode(IdleMode.kBrake);
+  r_motor3.setIdleMode(IdleMode.kBrake);
+  l_motor1.setIdleMode(IdleMode.kBrake);
+  l_motor2.setIdleMode(IdleMode.kBrake);
+  l_motor3.setIdleMode(IdleMode.kBrake);
+
+  //subsequent right motor follows next one
+  r_motor3.follow(r_motor2);
+  r_motor2.follow(r_motor1);
+  //subsequent left motor blah blah ablha
+  l_motor3.follow(l_motor2);
+  l_motor2.follow(l_motor1);
+
+  //inverting motors
+  r_motor1.setInverted(true);
+  l_motor1.setInverted(true);
+
+
+  }
+
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {}
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+
+// setting motor speeds
+   // r_motor1.set(0.5);
+   // r_motor2.set(0.5);
+   // r_motor3.set(0.5);
+   // l_motor1.set(0.5);
+   // l_motor2.set(0.5);
+   // l_motor3.set(0.5);
+    
+  //arcade
+  r_motor1.set(l_joystick.getY()-r_joystick.getX());
+  l_motor1.set(l_joystick.getY()+r_joystick.getX());
+  }
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}
